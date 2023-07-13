bd0af11c08218ff4ccda9d77cc3a8391aec36b8f
Eren Boran <erenn.borann@gmail.com>
2023-07-14 00:51:36 +0300

    LeafUpdate

---
+++ Assets/GameFolders/Scripts/LeafController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LeafController : MonoBehaviour
+{
+    [Range(0.1f, 5f)] [SerializeField] float _min = 0.1f;
+    [Range(6f, 15f)] [SerializeField] float _max = 15f;
+    
+
+    FrogController _leafPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    void Spawn()
+    {
+        FrogController newLeaf = Instantiate(_leafPrefab, transform.position, transform.rotation);
+
+    }
+}
